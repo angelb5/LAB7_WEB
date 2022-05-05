b1dbaa9e6b706d6e64dde818acdb18eac8a41a21
Angel <a20190192@pucp.edu.pe>
2022-05-04 19:09:10 -0500

    Detalles sesión

--- src/main/java/edu/pucp/gtics/lab5_gtics_20221/config/WebSecurityConfig.java
+++ src/main/java/edu/pucp/gtics/lab5_gtics_20221/config/WebSecurityConfig.java
@@ -29,7 +29,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.authorizeRequests()
                 .antMatchers("/plataformas/**","/distribuidoras/**").hasAuthority("ADMIN")
-                .antMatchers("carrito/**").hasAuthority("USER")
+                .antMatchers("/carrito/**").hasAuthority("USER")
                 .anyRequest().permitAll();
     }
 
